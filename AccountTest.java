9fd9dbc0eed4091a9dbac5614dd4da8d3b24fbb8
muhammaddzakiy34 <muhammad_dzakiy_34rpl@student.smktelkom-mlg.sch.id>
2025-08-21 07:00:45 +0700

    Update AccountTest.java

--- AccountTest.java
+++ AccountTest.java
@@ -1,7 +1,7 @@
 public class AccountTest {
     //Data pengguna bank
     public static void main(String[] args) {
-        //Creating instances of SavingsAccount with initial balance, interest rate, and customer name
+        //Membuat instance SavingsAccount dengan saldo awal, suku bunga, dan nama pelanggan
         SavingsAccount sa0001 = new SavingsAccount(0,0.04,"upin");
         sa0001.deposit (10000);
         sa0001.interestRate = 0.04;
@@ -11,17 +11,16 @@ public class AccountTest {
         sa0002.deposit (20000);
         sa0002.interestRate = 0.06;
         sa0002.customerName = "ipin";
-        //Displaying customer information
+        //Menampilkan informasi pelanggan
         System.out.println("Data Pengguna Bank: ");
         sa0001.displayCustomer();
         sa0002.displayCustomer();
-        //Calculating interest earned
+        //Menghitung bunga yang diperoleh
         double interestEarned1 = sa0001.balance * sa0001.interestRate;
         double interestEarned2 = sa0002.balance * sa0002.interestRate;
         System.out.println("Interest earned by " + sa0001.customerName + " : " + interestEarned1);
         System.out.println("Interest earned by " + sa0002.customerName + " : " + interestEarned2);
-        //Calculating and displaying the quotient of two numbers
-
+        //Menghitung dan menampilkan hasil bagi dua angka
     }
 
         
