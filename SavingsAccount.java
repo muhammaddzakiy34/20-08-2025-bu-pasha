a90ac834741617b35450c1df295002393d3ca305
muhammaddzakiy34 <muhammad_dzakiy_34rpl@student.smktelkom-mlg.sch.id>
2025-08-21 07:03:31 +0700

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -2,22 +2,22 @@ public class SavingsAccount {
     public int balance;
     public double interestRate;
     public String customerName;
-    //This constructor initializes the SavingsAccount with balance, interest rate, and customer name
+    //Konstruktor ini menginisialisasi SavingsAccount dengan saldo, suku bunga, dan nama pelanggan
     public SavingsAccount(int balance, double interestRate, String customerName) {
         balance = balance;
         interestRate = interestRate;
         customerName = customerName;
     }
-    //This method allows the user to deposit money into the account
+    //Konstruktor ini menginisialisasi SavingsAccount dengan saldo awal
     public void deposit(int amount) {
         balance += amount;
     }
-    //This method displays the customer's information
+    //Metode ini menampilkan informasi pelanggan
     public void displayCustomer() {
         System.out.println("Customer Name: " + customerName);
         System.out.println("Balance: " + balance);
         System.out.println("Interest Rate: " + interestRate);
-    // This method calculates the interest earned based on the current balance
+    //Metode ini menghitung bunga yang diperoleh berdasarkan saldo saat ini
     }
 }
      
